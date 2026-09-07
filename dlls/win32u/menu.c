a8c8e447fa168cf94125a30664dba2fe362d51ea
aabluedragon <aabluedragon@gmail.com>
2026-09-07 18:08:12 +0300

    win32u: paint popup before surface flush

--- dlls/win32u/menu.c
+++ dlls/win32u/menu.c
@@ -3137,6 +3137,7 @@ static BOOL show_popup( HWND owner, HMENU hmenu, UINT id, UINT flags,
     flush_window_surfaces( TRUE );
     NtUserShowWindow( menu->hWnd, SW_SHOWNOACTIVATE );
     NtUserRedrawWindow( menu->hWnd, NULL, 0, RDW_INVALIDATE | RDW_UPDATENOW | RDW_ALLCHILDREN );
+    send_message( menu->hWnd, WM_PAINT, 0, 0 );
     flush_window_surfaces( TRUE );
     return TRUE;
 }
